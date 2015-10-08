2a0e405b82282ff6c0090da756a0802cbc1a471a
Sergio <sergio.rovira96@gmail.com>
2015-10-08 23:03:00 +0200

    Afegir el programa

---
+++ ContarLletres.java
@@ -0,0 +1,36 @@
+package net.sergio.contarlletres;
+
+public class ContarLletres {
+
+	private static final String STRING = "Hola hola hola";
+	
+	public static void main(String[] args) {
+		String lletres ="";
+		for (int i = 0; i<STRING.length();i++) {
+			char temp = STRING.charAt(i);
+			if(!(comprovaRepetides(temp, lletres))) {
+				lletres = lletres + temp;
+			}
+		}
+		lletresRepetides(STRING, lletres);
+	}
+	private static void lletresRepetides(String string2, String lletres) {
+		for (int i = 0; i<lletres.length();i++) {
+			int contador = 0;
+			for (int j = 0; j<STRING.length();j++) {
+				if (lletres.charAt(i)==STRING.charAt(j)) {
+					contador ++;
+				}
+			}
+			System.out.println("La lletra " + lletres.charAt(i) + " es repeteix " + contador + " vegades");
+		}
+	}
+	private static boolean comprovaRepetides(char temp, String lletres) {
+		String lletra = "" + temp;
+		if (lletra.equals(" ")) return true;
+		if (lletres.indexOf(temp)>0) {
+			return true;
+		}
+		return false;
+	}
+}
